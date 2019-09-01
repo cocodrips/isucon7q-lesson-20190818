5324a559b11a4614e9bea58ee13dbfd87ec9af91
cocodrips <cocodrips@gmail.com>
2019-09-01 18:42:09 +0900

    change icon path

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -766,7 +766,7 @@ func main() {
 
 	e.GET("add_channel", getAddChannel)
 	e.POST("add_channel", postAddChannel)
-	e.GET("/icons/:file_name", getIcon)
+	//e.GET("/icons/:file_name", getIcon)
 
 	e.Start(":5000")
 }
